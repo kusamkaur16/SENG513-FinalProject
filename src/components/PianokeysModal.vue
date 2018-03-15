80982af50010e3661e0c2193fb32663d992d9de2
kusamkaur16 <kusamdeepbrar@gmail.com>
2018-03-14 21:30:04 -0600

    basic UI added

--- src/components/PianokeysModal.vue
+++ src/components/PianokeysModal.vue
@@ -1,101 +1,134 @@
 <template>
+<div class="footer">
+    <button class="decreaseOctave btn btn-outline-primary"><</button>
+    <div class="pianoKeys">
+      <div class="key white c"></div>
+      <div class="key black cs"></div>
+      <div class="key white d"></div>
+      <div class="key black ds"></div>
+      <div class="key white e"></div>
+      <div class="key white f"></div>
+      <div class="key black fs"></div>
+      <div class="key white g"></div>
+      <div class="key black gs"></div>
+      <div class="key white a"></div>
+      <div class="key black as"></div>
+      <div class="key white b"></div>
+      <div class="key white c"></div>
+      <div class="key black cs"></div>
+      <div class="key white d"></div>
+      <div class="key black ds"></div>
+      <div class="key white e"></div>
+      <div class="key white f"></div>
+      <div class="key black fs"></div>
+      <div class="key white g"></div>
+      <div class="key black gs"></div>
+      <div class="key white a"></div>
+      <div class="key black as"></div>
+      <div class="key white b"></div>
+    </div>
+    <button class="increaseOctave btn btn-outline-primary">></button>
 
-    <ul class="set">
-
-      <li class="white c"></li>
-      <li class="black cs"></li>
-      <li class="white d"></li>
-      <li class="black ds"></li>
-      <li class="white e"></li>
-      <li class="white f"></li>
-      <li class="black fs"></li>
-      <li class="white g"></li>
-      <li class="black gs"></li>
-      <li class="white a"></li>
-      <li class="black as"></li>
-      <li class="white b"></li>
-
-    </ul>
-
+</div>
 </template>
 <script src="https://github.com/Tonejs/Tone.js/blob/master/examples/scripts/Keyboard.js"></script>
 <script src="../js/keys.js"></script>
 
 <style>
-
-
-ul {
-    height: 18.875em;
-    width: 34em;
-    margin: 5em auto;
-    padding: 3em 0 0 3em;
-    position: relative; //  border: 1px solid #160801;
-    bottom: 10px;
-    // border-radius: 1em;
-    // background: -webkit-gradient(linear, right bottom, left top, color-stop(0%, rgba(0, 0, 0, 0.3)), color-stop(100%, rgba(0, 0, 0, 0))), url('https://f.cl.ly/items/2q1f3t0C3R1b0g081w3n/vwood.png');
-    // box-shadow: 0 0 50px rgba(0, 0, 0, 0.5) inset, 0 1px rgba(212, 152, 125, 0.2) inset, 0 5px 15px rgba(0, 0, 0, 0.5);
-    box-shadow: 0 0 50px rgba(0, 123, 255, 0.5) inset, 0 1px rgba(212, 152, 125, 0.2) inset, 0 5px 15px rgba(0, 0, 0, 0.5);
+.footer {
+  height: 20%;
+  width: 100%;
+  position: absolute;
+  bottom: 0px;
 }
 
-li {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    position: relative;
-    float: left;
+.increaseOctave {
+    position: absolute;
+right: 0px;
+bottom: 0px;
+width: 5%;
+height: 100%;
 }
 
-ul .white {
-    height: 16em;
-    width: 4em;
-    z-index: 1;
-    border-left: 1px solid #bbb;
-    border-bottom: 1px solid #bbb;
-    border-radius: 0 0 5px 5px;
-    box-shadow: -1px 0 0 rgba(255, 255, 255, 0.8) inset, 0 0 5px #ccc inset, 0 0 3px rgba(0, 0, 0, 0.2);
-    background: linear-gradient(top, #eee 0%, #fff 100%);
+.decreaseOctave {
+    position: absolute;
+    left: 0px;
+    bottom: 0px;
+    width: 5%;
+height: 100%;
+}
+.pianoKeys {
+  height: 100%;
+  width: 90%;
+  left:5%;
+      padding-left: 1%;
+  /* margin: 5em auto;
+  padding: 3em 0 0 3em; */
+  position: relative;
+
+  /* bottom: 10px; // border-radius: 1em;
+  // background: -webkit-gradient(linear, right bottom, left top, color-stop(0%, rgba(0, 0, 0, 0.3)), color-stop(100%, rgba(0, 0, 0, 0))), url('https://f.cl.ly/items/2q1f3t0C3R1b0g081w3n/vwood.png');
+  // box-shadow: 0 0 50px rgba(0, 0, 0, 0.5) inset, 0 1px rgba(212, 152, 125, 0.2) inset, 0 5px 15px rgba(0, 0, 0, 0.5);
+  box-shadow: 0 0 50px rgba(0, 123, 255, 0.5) inset, 0 1px rgba(212, 152, 125, 0.2) inset, 0 5px 15px rgba(0, 0, 0, 0.5); */
 }
 
-ul .white:active
-{
-    border-top: 1px solid #777;
-    border-left: 1px solid #999;
-    border-bottom: 1px solid #999;
-    box-shadow: 2px 0 3px rgba(0, 0, 0, 0.1) inset, -5px 5px 20px rgba(0, 0, 0, 0.2) inset, 0 0 3px rgba(0, 0, 0, 0.2);
-    background: linear-gradient(top, #fff 0%, #e9e9e9 100%);
+.key {
+  margin: 0;
+  padding: 0;
+  position: relative;
+  float: left;
+}
+
+.white {
+    top:2.5%;
+    height: 95%;
+    width: 7.14%;
+  z-index: 1;
+  border-left: 1px solid #bbb;
+  border-bottom: 1px solid #bbb;
+  border-radius: 0 0 5px 5px;
+  box-shadow: -1px 0 0 rgba(255, 255, 255, 0.8) inset, 0 0 5px #ccc inset, 0 0 3px rgba(0, 0, 0, 0.2);
+  background: linear-gradient(top, #eee 0%, #fff 100%);
+}
+
+.white:active {
+  border-top: 1px solid #777;
+  border-left: 1px solid #999;
+  border-bottom: 1px solid #999;
+  box-shadow: 2px 0 3px rgba(0, 0, 0, 0.1) inset, -5px 5px 20px rgba(0, 0, 0, 0.2) inset, 0 0 3px rgba(0, 0, 0, 0.2);
+  background: linear-gradient(top, #fff 0%, #e9e9e9 100%);
 }
 
 .black {
-    height: 8em;
-    width: 2em;
-    margin: 0 0 0 -1em;
-    z-index: 2;
-    border: 1px solid #000;
-    border-radius: 0 0 3px 3px;
-    box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.2) inset, 0 -5px 2px 3px rgba(0, 0, 0, 0.6) inset, 0 2px 4px rgba(0, 0, 0, 0.5);
-    background: linear-gradient(45deg, #222 0%, #555 100%);
+    top:2.5%;
+  height: 54%;
+  width: 2.75%;
+  margin: 0 0 0 -1em;
+  z-index: 2;
+  border: 1px solid #000;
+  border-radius: 0 0 3px 3px;
+  box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.2) inset, 0 -5px 2px 3px rgba(0, 0, 0, 0.6) inset, 0 2px 4px rgba(0, 0, 0, 0.5);
+  background: linear-gradient(45deg, #222 0%, #555 100%);
 }
 
 .black:active {
-    box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.2) inset, 0 -2px 2px 3px rgba(0, 0, 0, 0.6) inset, 0 1px 2px rgba(0, 0, 0, 0.5);
-    background: linear-gradient(left, #444 0%, #222 100%);
+  box-shadow: -1px -1px 2px rgba(255, 255, 255, 0.2) inset, 0 -2px 2px 3px rgba(0, 0, 0, 0.6) inset, 0 1px 2px rgba(0, 0, 0, 0.5);
+  background: linear-gradient(left, #444 0%, #222 100%);
 }
 
 .a,
 .b,
 .e,
 .g,
-.d,
-.c {
-    margin: 0 0 0 -1em;
+.d{
+  margin: 0 0 0 -1.5%;
 }
 
-ul li:first-child {
-    border-radius: 5px 0 5px 5px;
+.key:first-child {
+  border-radius: 5px 0 5px 5px;
 }
 
-ul li:last-child {
-    border-radius: 0 5px 5px 5px;
+.key:last-child {
+  border-radius: 0 5px 5px 5px;
 }
-
 </style>
--- src/js/keys.js
+++ src/js/keys.js
@@ -1,5 +1,5 @@
 var Tone = require('tone');
-
+var octave = 1;
 var btn;
 var synth;
 $(document).ready(function() {
@@ -15,14 +15,6 @@ $(document).ready(function() {
   document.body.addEventListener('keypress', function(e) {
     //    console.log('key code is ', e.keyCode);
     if (e.keyCode == 97) { //'a' key
-        //$(".c").trigger('click');
-//         document.getElementsByClassName('c')[0].click();
-//         document.getElementsByClassName('c')[0].removeClass("active");
-// // add class to the one we clicked
-//         $(this).addClass("active");
-
-
-      //    $(".c").css('background-color', 'red');
       synth.triggerAttackRelease('C4', '0.02');
     }
     if (e.keyCode == 119) { //'w' key
@@ -91,6 +83,43 @@ $(document).ready(function() {
     synth.triggerAttackRelease('A4', '0.02');
   });
   $(".as").click(function() {
+    synth.triggerAttackRelease('A#4', '0.02');
+  });
+  $(".b").click(function() {
+    synth.triggerAttackRelease('B4', '0.02');
+  });
+
+  $(".c1").click(function() {
+    synth.triggerAttackRelease('C4', '0.02');
+  });
+  $(".c1s").click(function() {
+    synth.triggerAttackRelease('C#4', '0.02');
+  });
+  $(".d1").click(function() {
+    synth.triggerAttackRelease('D4', '0.02');
+  });
+  $(".d1s").click(function() {
+    synth.triggerAttackRelease('D#4', '0.02');
+  });
+  $(".e1").click(function() {
+    synth.triggerAttackRelease('E4', '0.02');
+  });
+  $(".f1").click(function() {
+    synth.triggerAttackRelease('F4', '0.02');
+  });
+  $(".f1s").click(function() {
+    synth.triggerAttackRelease('F#4', '0.02');
+  });
+  $(".g1").click(function() {
+    synth.triggerAttackRelease('G4', '0.02');
+  });
+  $(".g1s").click(function() {
+    synth.triggerAttackRelease('G#4', '0.02');
+  });
+  $(".a1").click(function() {
+    synth.triggerAttackRelease('A4', '0.02');
+  });
+  $(".a1s").click(function() {
     synth.triggerAttackRelease('A#4', '0.02');
   });
   $(".b").click(function() {
