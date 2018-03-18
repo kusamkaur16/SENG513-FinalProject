67db0956b6576975d7585d72076d3efec38e933e
sjhepbur <sjhepbur@ucalgary.ca>
2018-03-18 17:03:47 -0600

    Adding simple functionality that will change the users picture when button is clicked.

--- src/components/AccountModal.vue
+++ src/components/AccountModal.vue
@@ -11,8 +11,10 @@
         </div>
         <div class="modal-body row">
           <div class="col-4">
-            <img src="../assets/logo.png" alt="User Picture" width="150" height="150">
-            <button type="button" class="btn btn-outline-primary">Change Picture</button>
+            <img id ="image" :src="defaultImage" alt="User Picture" width="150" height="150">
+            <input id="uploadPicture" accept="image/x-png,image/gif,image/jpeg" type="file">
+            <label for="uploadPicture" class="btn btn-outline-primary" v-on:click="changeImage">Change Picture</label>
+            <!-- <button type="file" class="btn btn-outline-primary" v-on:click="changeImage">Change Picture</button> -->
           </div>
           <div class="col-8">
             <h5>My Compositions</h5>
@@ -34,6 +36,7 @@
 </template>
 
 <script>
+import picture from '../assets/tmp.png'
 export default {
   name: 'account-modal',
   data () {
@@ -43,7 +46,18 @@ export default {
         {name: 'composition 2'},
         {name: 'composition 3'},
         {name: 'composition 4'}
-      ]
+      ],
+      defaultImage: require('../assets/logo.png')
+    }
+  },
+  methods: {
+    changeImage: function () {
+      var img = document.getElementById('image')
+      var input = document.getElementById('uploadPicture').value
+      console.log(input)
+      img.src = require('../assets/tmp.png')
+      this.defaultImage = picture
+      // console.log(img.src)
     }
   }
 }
@@ -61,5 +75,8 @@ export default {
   #scrollComps {
     height: 200px;
     overflow-y: scroll;
+  }
+  #uploadPicture {
+    display: none;
   }
 </style>
