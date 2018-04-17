50bb5ff1758abc18803fb309dc33170a03fe9dc3
askariya <arunaluk@yahoo.ca>
2018-04-16 18:37:45 -0600

    Resolving eslint errors

--- src/components/PianoKeys.vue
+++ src/components/PianoKeys.vue
@@ -304,22 +304,20 @@ export default {
     })
   },
   updated: function () {
-    try{
-        let rect = document.getElementsByClassName('C')[0].getBoundingClientRect()
-        let rect2 = document.getElementsByClassName('B1')[0].getBoundingClientRect()
+    try {
+      let rect = document.getElementsByClassName('C')[0].getBoundingClientRect()
+      let rect2 = document.getElementsByClassName('B1')[0].getBoundingClientRect()
 
-        // Calculates how long the keyboard is, and recenters it in between the change octave buttons
-        let keysDivWidth = document.getElementById('pianoKeys').offsetWidth
-        let leftAmount = (keysDivWidth - (rect2.right - rect.left)) / 2
-        let pianoKeyselem = document.getElementById('pianoKeys')
+      // Calculates how long the keyboard is, and recenters it in between the change octave buttons
+      let keysDivWidth = document.getElementById('pianoKeys').offsetWidth
+      let leftAmount = (keysDivWidth - (rect2.right - rect.left)) / 2
+      let pianoKeyselem = document.getElementById('pianoKeys')
 
-        // recenters the keyboard on resize
-        pianoKeyselem.style.left = (leftAmount) + 'px'
+      // recenters the keyboard on resize
+      pianoKeyselem.style.left = (leftAmount) + 'px'
+    } catch (error) {
+      // Do nothing
     }
-    catch (error){
-        // Do nothing 
-    }
-
   },
   methods: {
     // Handles changing the size of the keyboard when the window is resized
